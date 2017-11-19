cb58886eddee94c044435235cefc32bb2e7d4232
Neethu Mohandas <prayaganeethu@gmail.com>
2017-11-19 11:43:01 +0530

    Added some points as comments

--- modules/builtInVSmyOwnModules.js
+++ modules/builtInVSmyOwnModules.js
@@ -1,4 +1,13 @@
-// reference : https://www.w3schools.com/nodejs/nodejs_modules.asp
+/*
+References :-
+https://www.w3schools.com/nodejs/nodejs_modules.asp
+https://medium.freecodecamp.org/javascript-modules-a-beginner-s-guide-783f7d7a5fcc
+
+Using CommonJS approach here to design and import modules.
+CommonJS takes a server-first approach and synchronously loads modules.
+This matters because if we have three other modules we need to require,
+it’ll load them one by one
+*/
 
 const http = require('http')
 const timeStamp = require('./dateTime')
