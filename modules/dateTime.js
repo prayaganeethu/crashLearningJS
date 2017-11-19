392e6b9d42d17994dec079342c58a305cb5b4a2f
Neethu Mohandas <prayaganeethu@gmail.com>
2017-11-19 13:12:50 +0530

    Learning the difference between exports and module.exports

--- modules/dateTime.js
+++ modules/dateTime.js
@@ -1,3 +1,8 @@
-exports.giveDateTime = function () {
+// https://blog.tableflip.io/the-difference-between-module-exports-and-exports/
+
+let giveDateTime = function () {
   return Date()
 }
+
+exports = giveDateTime
+console.log(exports, module.exports)
